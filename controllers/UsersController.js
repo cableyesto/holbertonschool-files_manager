128de855abe0c9cc7ce03485c41bef21f1b2ccc9
cableyesto <dialkins@tutamail.com>
2026-01-07 11:22:44 +0100

    feat: Create the /user end point to add a user to the db.

---
+++ controllers/UsersController.js
@@ -0,0 +1,42 @@
+import crypto from 'crypto';
+import dbClient from '../utils/db';
+
+const postNew = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: 'Missing email' });
+  }
+
+  if (!password) {
+    return res.status(400).json({ error: 'Missing password' });
+  }
+  try {
+    const usersCollection = dbClient.db.collection('users');
+    const existingUser = await usersCollection.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = crypto
+      .createHash('sha1')
+      .update(password)
+      .digest('hex');
+
+    const result = await usersCollection.insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      id: result.insertedId,
+      email,
+    });
+  } catch (error) {
+    console.error('Error creating user:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export default postNew;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { getStats, getStatus } from '../controllers/AppController';
+import postNew from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', getStatus);
 
 router.get('/stats', getStats);
+
+router.post('/users', postNew);
 
 export default router;
